4becd44f192de5af95cb7acdfd22ae94c438712c
Wojciech Dziurowicz <wdziurowicz@gmail.com>
2020-04-03 16:45:52 +0200

    added dataset analysis

--- heart_disease_classifiers.py
+++ heart_disease_classifiers.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 # %% import dataset - UCI Heart Disease 
 data = pd.read_csv('heart.csv')
 
@@ -31,6 +32,23 @@ X = pd.get_dummies(X, columns=cat_val)
 
 X.head(5)
 
+# %% dataset analysis - correlation matrix
+corr_matrix = data.corr()
+fig, ax = plt.subplots(figsize=(15, 15))
+ax = sns.heatmap(corr_matrix,
+                 annot=True,
+                 linewidths=1,
+                 fmt=".2f")
+ymin, ymax = ax.get_ylim()
+ax.set_ylim(ymin + 0.5, ymax - 0.5)
+# %% dataset analysis - independent variables correlation with target
+x_corrs = data.drop('target', axis=1).corrwith(data.target)
+sns.set_style("whitegrid")
+fig, ax = plt.subplots(figsize=(15, 15))
+ax = sns.barplot(x=x_corrs.index,
+                 y=x_corrs.values,
+                 palette='rocket',
+                 linewidth=1)
 # %% feature scaling
 from sklearn.preprocessing import StandardScaler
 
@@ -75,7 +93,7 @@ random_forest.fit(X_train, y_train)
 
 y_pred = random_forest.predict(X_test)
 
-# Making the Confusion Matrix
+# %% make confusion matrix
 from sklearn.metrics import confusion_matrix
 cm_rf = confusion_matrix(y_test, y_pred)
 
